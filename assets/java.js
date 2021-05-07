137e9d195aced09875b78587a1f7bc0f33d4a73c
michaelab24 <michaelbarnes459@yahoo.com>
2021-05-07 08:52:12 -0500

    html created, begin java work

---
+++ assets/java.js
@@ -0,0 +1,19 @@
+var startButton = document.getElementById("start")
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
+
+
+function buildQuiz(){}
+
+function startQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+startButton.addEventListener ("click", startQuiz)
+submitButton.addEventListener("click", showResults);
+
